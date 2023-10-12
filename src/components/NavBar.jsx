0a53cc33c93dd7088e48dc35ca9724f42ea886ab
hirwa sean <kdsean11@gmail.com>
2023-10-12 14:55:48 +0200

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const NavBar = () => {
+    return ( 
+        <>
+        <div className="navbar bg-black underline">
+        <h1 className="bg-green-800">hello world</h1>
+        </div>
+        </>
+     );
+}
+ 
+export default NavBar;
