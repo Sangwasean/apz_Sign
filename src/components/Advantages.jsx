5ea63e85550ab757acbec9506ff90c35305b72c6
hirwa sean <kdsean11@gmail.com>
2023-10-13 23:46:29 +0200

    Working on navigation bar

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const Advantages = () => {
   return (
     <>
-      <div className="">
-        <h1 className="">
-          Seamless eSignature experience anytime and anywhere, on any device
-        </h1>
-      </div>
+      <div className=""></div>
     </>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 import { useState } from "react";
+import { FaArrowDown ,FaArrowUp} from "react-icons/fa";
 const NavBar = () => {
-    const [isOpen,setIsOpen]=useState();
-    return ( 
-        <>
-        <div className="navbar fixed drop-shadow-xl flex  items-center p-10 top-0 bg-slate-50 w-full h-6 font-semibold font-['Arial'] justify-center">
-            <ul className=" flex  items-center flex-row space-x-5">
-                <li className="mr-4 font-['Georgia'] text-2xl text-red-500">APZ-Sign</li>
-                <li  className="mr-4 cursor-pointer hover:text-red-500 "><a hover={()=>{setIsOpen((prev => !prev))}}>Features</a></li>
-                <li className="mr-4 cursor-pointer hover:text-red-500"><a>Solutions</a></li>
-                <li className="mr-4 cursor-pointer hover:text-red-500"><a>API</a></li>
-                <li className="mr-4 cursor-pointer hover:text-red-500"><a>Samples</a></li>
-                <button className="mr-4 border-4 rounded-lg border-red-500 p-2 ml-20 ">Login</button>
-                <button className="r rounded-lg  p-2 bg-red-500 text-white font-medium font-['Proxima Nova'] " >Signup</button>
-            </ul>
-        </div>
-        </>
-     );
-}
- 
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <div className="navbar fixed drop-shadow-xl flex p-10 top-0 bg-slate-50 w-screen h-6 font-semibold font-['Arial']">
+        <ul className=" flex  items-center flex-row space-x-5">
+          <li className="mr-4 font-['Georgia'] text-2xl text-red-500">
+            APZ-Sign
+          </li>
+          <li className="mr-4 cursor-pointer hover:text-red-500">
+            <a className="flex" onClick={() => setIsOpen((prev) => !prev)}>
+              Features
+              {!isOpen ? (
+                <FaArrowDown className="h-6 ml-2" />
+              ) : (
+                <FaArrowUp className="h-6 ml-2"/>
+              )}
+            </a>
+          </li>
+          <li className="mr-4 cursor-pointer hover:text-red-500">
+            <a>Solutions</a>
+          </li>
+          <li className="mr-4 cursor-pointer hover:text-red-500">
+            <a>API</a>
+          </li>
+          <li className="mr-4 cursor-pointer hover:text-red-500">
+            <a>Samples</a>
+          </li>
+          <div className="relative left-96">
+            <button className="mr-4 border-4 rounded-lg border-red-500 p-2  ">
+              Login
+            </button>
+            <button className="r rounded-lg  p-2 bg-red-500 text-white font-medium font-['Proxima Nova'] ">
+              Signup
+            </button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+};
+
 export default NavBar;
